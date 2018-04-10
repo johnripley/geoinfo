686782f4ac502268638a2066f791b2ddfb3b827a
johnripley <johnripley@fico.com>
2018-04-10 14:46:08 -0500

    fixed unit test

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # geoinfo
 
-US zip code coordinates file (src/main/resources/zip-coords.txt copied and renamed from https://gist.github.com/erichurst/7882666
+US zip code coordinates file (src/main/resources/zip-coords.txt) copied and renamed from https://gist.github.com/erichurst/7882666
--- src/test/java/org/johnripley/geoinfo/BaseDistanceCalculatorTest.java
+++ src/test/java/org/johnripley/geoinfo/BaseDistanceCalculatorTest.java
@@ -7,13 +7,34 @@ import org.junit.Test;
 public class BaseDistanceCalculatorTest {
 
 	@Test
-	public void testLoad() {
+	public void testZip() {
 		DistanceCalculator lu = new BaseDistanceCalculator();
 		Distance d = lu.getDistance("78681", "78729");
 		System.out.println(d.getPointA());
 		System.out.println(d.getPointB());
 		System.out.println(d.getDistance());
-		assertEquals(21.1,d.getDistance(),0.1);
+		assertEquals(8.91,d.getDistance(),0.01);
 	}
 
+	@Test
+	public void testCoords() {
+		DistanceCalculator lu = new BaseDistanceCalculator();
+		Distance d = lu.getDistance(new BaseCoordinate(30.534424,-97.725741), new BaseCoordinate(30.458396,-97.755344));
+		System.out.println(d.getPointA());
+		System.out.println(d.getPointB());
+		System.out.println(d.getDistance());
+		assertEquals(8.91,d.getDistance(),0.01);
+	}
+	
+	@Test
+	public void testRawCoords() {
+		DistanceCalculator lu = new BaseDistanceCalculator();
+		Distance d = lu.getDistance(30.534424,-97.725741,30.458396,-97.755344);
+		System.out.println(d.getPointA());
+		System.out.println(d.getPointB());
+		System.out.println(d.getDistance());
+		assertEquals(8.91,d.getDistance(),0.01);
+	}
+
+	
 }
